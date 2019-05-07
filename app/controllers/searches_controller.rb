b2a7be6cbbd73f1c2c0dd60baace7c9225fb52b7
George  Pianka <gpianka1@gmail.com>
2019-05-07 06:05:37 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,5 +11,18 @@ class SearchesController < ApplicationController
   def foursquare
   
     foursquare = FoursquareService.new
-	    body = foursquare.foursquare(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], params[:zipcode])
-end
+	    
+	  body = foursquare.foursquare(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], params[:zipcode])
+
+    if !!@body
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+
+    render 'search'
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+      render 'search'
+    end
