cd7f87181a38901b1d826d83980bf4e28423cc59
George  Pianka <gpianka1@gmail.com>
2019-05-07 05:57:03 +0000

    Automatically backed up by Learn

--- app/services/foursquare_sevice.rb
+++ app/services/foursquare_sevice.rb
@@ -1,5 +1,5 @@
 class FoursquareService
-  def authenticate(client_id, client_secret, code)
+   def authenticate(client_id, client_secret, code)
       resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
           req.params['client_id'] = client_id
           req.params['client_secret'] = client_secret
@@ -10,16 +10,16 @@ class FoursquareService
 
        body = JSON.parse(resp.body)
       body["access_token"]
-  end    
+   end    
 
-  def friends(token)
+   def friends(token)
       resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
           req.params['oauth_token'] = token
           # don't forget that pesky v param for versioning
           req.params['v'] = '20160201'
         end
         JSON.parse(resp.body)["response"]["friends"]["items"]
-  end
+   end
 
    def foursquare(client_id, client_secret, zipcode)
       @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
