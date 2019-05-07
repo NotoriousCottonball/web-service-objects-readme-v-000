d3a26a58a316572f4958d6026178b8db39d27443
George  Pianka <gpianka1@gmail.com>
2019-05-07 05:59:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
 
   def create
    foursquare = FoursquareService.new
+   
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']	    session[:token] = foursquare.authenticate(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
+      
     redirect_to root_path
   end
 end
