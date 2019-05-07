0479b9f308d030038eb97651913c1724b83169db
George  Pianka <gpianka1@gmail.com>
2019-05-07 06:03:52 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,9 @@ class SearchesController < ApplicationController
     foursquare = FoursquareService.new
     @friends = foursquare.friends(session[:token])
   end
-
-  oursquare = FoursquareService.new
+  
+  def foursquare
+  
+    foursquare = FoursquareService.new
     client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"	    body = foursquare.foursquare(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], params[:zipcode])
 end
