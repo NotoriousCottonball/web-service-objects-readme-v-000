87b70fe3fc561654fa0bd401094c9a06359f8444
George  Pianka <gpianka1@gmail.com>
2019-05-07 06:01:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
    session[:token] = foursquare.authenticate(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
       
     redirect_to root_path
+    
   end
   
 end
